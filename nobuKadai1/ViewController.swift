848578ca65c5b86716eeef64a463dcf8666299b1
Atsunobu Yuge <yugechan666@gmail.com>
2021-11-11 23:58:10 +0900

    Initial Commit

---
+++ nobuKadai1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  nobuKadai1
+//
+//  Created by 弓削　敦信 on 2021/11/11.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
