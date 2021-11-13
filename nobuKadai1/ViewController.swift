8118f2a462449d528ce1352cd9e5d8447657f011
Atsunobu Yuge <yugechan666@gmail.com>
2021-11-13 10:04:18 +0900

    一旦完成

--- nobuKadai1/ViewController.swift
+++ nobuKadai1/ViewController.swift
@@ -9,11 +9,29 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet weak var textField1: UITextField!
+    @IBOutlet weak var textField2: UITextField!
+    @IBOutlet weak var textField3: UITextField!
+    @IBOutlet weak var textField4: UITextField!
+    @IBOutlet weak var textField5: UITextField!
+    
+    @IBOutlet weak var label: UILabel!
+    
+    @IBAction func addButton(_ sender: Any) {
+        let number1 = Int(textField1.text ?? "") ?? 0
+        let number2 = Int(textField2.text ?? "") ?? 0
+        let number3 = Int(textField3.text ?? "") ?? 0
+        let number4 = Int(textField4.text ?? "") ?? 0
+        let number5 = Int(textField5.text ?? "") ?? 0
+
+        let result = number1 + number2 + number3 + number4 + number5
+        
+        label.text = "\(result)"
+    }
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
     }
-
 
 }
 
